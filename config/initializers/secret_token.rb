af9b5b91b3ab00b00d2de59da7a70ff59138a7cc
Joseph Barrozo <JABarrozO@gmail.com>
2012-04-25 16:04:48 +0800

    first commit, yay!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poop::Application.config.secret_token = '8a112ea59508b02feaffc65d70878d2e1c39c894b3f3564e979cfdf0d026b7d3c960b63156643e855984a15b61b849dfdfc715b782dc82c3cca4106f8bdd152b'
